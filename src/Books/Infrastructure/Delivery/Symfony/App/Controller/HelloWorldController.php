0895a1328b570c03936a952fae15ad7e8b8fd379
Pedro Giménez <me@pedro.bz>
2019-12-10 20:21:48 +0100

    Install annotations and create a test controller

---
+++ src/Books/Infrastructure/Delivery/Symfony/App/Controller/HelloWorldController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Books\Infrastructure\Delivery\Symfony\App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloWorldController extends AbstractController
+{
+    /** @route("/") */
+    public function index()
+    {
+        return new Response("Hello, world!");
+    }
+}
