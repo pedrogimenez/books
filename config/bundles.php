642bebad516f5ca99747b4986556a5e34c28d719
Pedro Giménez <me@pedro.bz>
2019-12-11 15:41:11 +0100

    Remove root config directory

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-];
